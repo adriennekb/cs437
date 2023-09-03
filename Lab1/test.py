70c0bfca9c518162e424275be23515130fc6812b
Raoul <6421655+Raove@users.noreply.github.com>
2023-09-03 10:13:34 -0700

    Create test.py

---
+++ Lab1/test.py
@@ -0,0 +1,37 @@
+import picar_4wd as fc
+import time
+import collections
+
+speed = 20
+
+def test():
+    # List to store distances
+    scan_distances_list = collections.deque([], maxlen=4)
+    fc.forward(speed)
+
+    while True:
+        # Find distance, then turn right if close
+        distance = fc.get_distance_at(0)
+        print(distance)
+        scan_distances_list.append(distance)
+
+        if distance < 12.0:
+            print("Move back and turn right")
+            # Move back and turn right
+            fc.backward(speed)
+            time.sleep(.3)
+            fc.turn_right(speed*2)
+            time.sleep(2)
+            # Stop and restart loop to go forward
+            fc.stop()
+        else:
+            fc.forward(speed)
+        time.sleep(.1)
+
+
+if __name__ == '__main__':
+    try:
+        test()
+    except KeyboardInterrupt:
+        fc.stop()
+        exit(0)
