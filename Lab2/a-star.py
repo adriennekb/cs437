a5014162bc746e86c4143740223562da2e358279
Satvik Sethia <satviksethia@Satviks-Air.lan>
2023-09-15 01:15:37 -0400

    Add astar impl

---
+++ Lab2/a-star.py
@@ -0,0 +1,124 @@
+import heapq
+
+
+def manhattan(start_row, start_col, end_row, end_col):
+  return (abs(end_row - start_row) + abs(end_col - start_col))
+
+
+def construct_path(current_pos, predecessor):
+  path = [current_pos]
+
+  while current_pos in predecessor.keys():
+    current_pos = predecessor[current_pos]
+    path.append(current_pos)
+
+  path.reverse()
+  return path
+
+
+def getNeighbors(current_pos, grid):
+  current_row = current_pos[0]
+  current_col = current_pos[1]
+  possible_neighbors = [(current_row + 1, current_col), 
+               (current_row - 1, current_col), 
+               (current_row, current_col - 1), 
+               (current_row, current_col - 1)]
+  neighbors = []
+  
+  for neighbor in possible_neighbors:
+    if neighbor[0] < 0 or neighbor[0] >= len(grid) or neighbor[1] < 0 or neighbor[1] >= len(grid[0]) or grid[neighbor[0]][neighbor[1]] != 0:
+      continue
+    new_dir = (neighbor[0] - current_pos[0]) + ((neighbor[1] - current_pos[1]) * 0.5)
+    neighbors.append((neighbor, new_dir))
+  
+  return neighbors
+
+
+def getDirPriority(curr_dir, new_dir):
+  dir_diff = curr_dir - new_dir
+  if dir_diff == 0:
+    return 0
+  if dir_diff % 0.5 == 0:
+    return 0.5
+  return 1
+
+def getStart(grid):
+  for i in range(0, len(grid)):
+    for j in range(0, len(grid[i])):
+      if grid[i][j] == 2:
+        return (i,j)
+
+
+def astar(grid):
+  start_pos = getStart(grid)
+  # Defining end as top-left of grid, can be changed 
+  end_pos = (0, 0)
+  
+  frontier = []
+  predecessor = {}
+  direction = {
+    start_pos: -1
+  }
+  g_cost = {
+    start_pos: 0
+  }
+  f_cost = {
+    start_pos: manhattan(start_pos[0], start_pos[1], end_pos[0], end_pos[1])
+  }
+
+  heap_order = 0
+  heapq.heappush(frontier, (f_cost[start_pos], 0, heap_order, start_pos))
+
+  while len(frontier) > 0:
+    current = heapq.heappop(frontier)
+    current_pos = current[3]
+    curr_dir = direction[current_pos]
+
+    if current_pos == end_pos:
+      return construct_path(current_pos, predecessor)
+    
+    neighbors = getNeighbors(current_pos, grid)
+    for neighbor_pos, neighbor_dir in neighbors:
+      neighbor_g = g_cost[current_pos] + 1
+      if neighbor_pos not in g_cost or neighbor_g < g_cost[neighbor_pos]:
+        neighbor_f = neighbor_g + manhattan(neighbor_pos[0], neighbor_pos[1], end_pos[0], end_pos[1])
+        g_cost[neighbor_pos] = neighbor_g
+        f_cost[neighbor_pos] = neighbor_f
+        predecessor[neighbor_pos] = current_pos
+        direction[neighbor_pos] = neighbor_dir
+        dir_diff = getDirPriority(curr_dir, neighbor_dir)
+        heap_order += 1
+        heapq.heappush(frontier, (f_cost[neighbor_pos], dir_diff, heap_order, neighbor_pos))
+
+
+if __name__ == '__main__':
+  print(astar([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]))
