1f209adc9898f67b58e830c3719fbaff943a22a4
Raoul <6421655+Raove@users.noreply.github.com>
2023-10-06 12:32:47 -0700

    Code from professor

--- Lab2/index.js
+++ Lab2/index.js
@@ -1,10 +1,13 @@
+document.onkeydown = updateKey;
+document.onkeyup = resetKey;
+
 var server_port = 65432;
 var server_addr = "192.168.1.53";   // the IP address of your Raspberry PI
 
 function client(){
     
     const net = require('net');
-    var input = document.getElementById("myName").value;
+    var input = document.getElementById("message").value;
 
     const client = net.createConnection({ port: server_port, host: server_addr }, () => {
         // 'connect' listener.
@@ -15,7 +18,7 @@ function client(){
     
     // get the data from the server
     client.on('data', (data) => {
-        document.getElementById("greet_from_server").innerHTML = data;
+        document.getElementById("bluetooth").innerHTML = data;
         console.log(data.toString());
         client.end();
         client.destroy();
@@ -28,14 +31,49 @@ function client(){
 
 }
 
-function greeting(){
+// for detecting which key is been pressed w,a,s,d
+function updateKey(e) {
 
-    // get the element from html
-    var name = document.getElementById("myName").value;
-    // update the content in html
-    document.getElementById("greet").innerHTML = "Hello " + name + " !";
-    // send the data to the server 
-    // to_server(name);
-    client();
+    e = e || window.event;
 
+    if (e.keyCode == '87') {
+        // up (w)
+        document.getElementById("upArrow").style.color = "green";
+        send_data("87");
+    }
+    else if (e.keyCode == '83') {
+        // down (s)
+        document.getElementById("downArrow").style.color = "green";
+        send_data("83");
+    }
+    else if (e.keyCode == '65') {
+        // left (a)
+        document.getElementById("leftArrow").style.color = "green";
+        send_data("65");
+    }
+    else if (e.keyCode == '68') {
+        // right (d)
+        document.getElementById("rightArrow").style.color = "green";
+        send_data("68");
+    }
+}
+
+// reset the key to the start state 
+function resetKey(e) {
+
+    e = e || window.event;
+
+    document.getElementById("upArrow").style.color = "grey";
+    document.getElementById("downArrow").style.color = "grey";
+    document.getElementById("leftArrow").style.color = "grey";
+    document.getElementById("rightArrow").style.color = "grey";
+}
+
+
+// update data for every 50ms
+function update_data(){
+    setInterval(function(){
+        // get image from python server
+        client();
+    }, 50);
 }
