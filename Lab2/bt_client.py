13476586732dfdba09b1730f3fc990648c5c335a
Raoul <6421655+Raove@users.noreply.github.com>
2023-10-06 18:42:45 -0700

    Update bt_client.py

--- Lab2/bt_client.py
+++ Lab2/bt_client.py
@@ -1,6 +1,6 @@
 import bluetooth
 
-host = "DC:A6:32:80:7D:87" # The address of Raspberry PI Bluetooth adapter on the server.
+host = "E4:5F:01:FC:E7:EE" # The address of Raspberry PI Bluetooth adapter on the server.
 port = 1
 sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 sock.connect((host, port))
