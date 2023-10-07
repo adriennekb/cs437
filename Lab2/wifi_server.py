37cbd755d417ca93c3dc5228c270ae1f274a2571
adriennekb <acaby9@gmail.com>
2023-10-06 22:04:45 -0700

    fixed car direction inputs take 2

--- Lab2/wifi_server.py
+++ Lab2/wifi_server.py
@@ -9,7 +9,7 @@ from picar_4wd.utils import pi_read
 
 HOST = "10.0.0.222" # IP address of your Raspberry PI
 PORT = 65432          # Port to listen on (non-privileged ports are > 1023)
-speed = 0
+speed = 20
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
@@ -24,16 +24,16 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             print(d_data)
             if data != b"":
                 print(data)
-            if d_data.fc == "87":
+            if "87" in d_data:
                 print("forwards")
                 fc.forward(speed)
-            elif d_data.fc == "83":
+            elif "83" in d_data:
                 print("backwards")
                 fc.backward(speed)
-            elif d_data.fc == "65":
+            elif "65" in d_data:
                 print("turn left")
                 fc.turn_left(speed)
-            elif d_data.fc == "68":
+            elif "68" in d_data:
                 print("turn right")
                 fc.turn_right(speed)
             else:
